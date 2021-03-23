0c6226c959b11f717b0139ec655e01ef79b5b93d
j-hm <thinkalonej@gmail.com>
2021-03-23 12:48:12 +0900

    0323_pizza

--- Java_005_Application/src/com/callor/apps/App_04.java
+++ Java_005_Application/src/com/callor/apps/App_04.java
@@ -6,23 +6,31 @@ public class App_04 {
 	public static void main(String[] args) {
 		int[] intClass = new int[5];
 		Random rnd = new Random();
+		int intTotal = 0;
 
 		for (int i = 0; i < 5; i++) {
 			intClass[i] = rnd.nextInt(10) + 50;
 		}
-		
+
 		int pizzaPcs = 6;
-		int pizzaBox = 0; 
-		
+
+		System.out.println("=====================");
+		System.out.println("인원수\t피자\t전체");
+		System.out.println("---------------------");
+
 		for (int i = 0; i < 5; i++) {
 			boolean bYes = intClass[i] % pizzaPcs == 0;
-			if(!bYes) {
+			int pizzaBox = intClass[i] / pizzaPcs;
+			if (!bYes) {
 				++pizzaBox;
 			}
+			System.out.printf("%d\t %d\t %d\n"
+							, intClass[i], pizzaBox, (pizzaBox * pizzaPcs));
+			intTotal += pizzaBox;
 		}
-		System.out.println("인원수\t피자\t전체");
-		System.out.println();
-		
+		System.out.println("---------------------");
+		System.out.println("전체 피자 박스 : " + intTotal);
+
 	}
 
 }
