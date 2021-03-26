0178c6cd5b39d91d8d7b828c6e10a23f0f5d5f58
j-hm <thinkalonej@gmail.com>
2021-03-26 16:43:20 +0900

    0326_chuga

--- Java_008_Application/src/com/callor/apps/App_02.java
+++ Java_008_Application/src/com/callor/apps/App_02.java
@@ -1,11 +1,12 @@
 package com.callor.apps;
 
-import com.callor.apps.service.ScoreServiceV1;
+
+import com.callor.apps.service.ScoreServiceV1_1;
 
 public class App_02 {
 	public static void main(String[] args) {
 		
-		ScoreServiceV1 ssV1 = new ScoreServiceV1();
+		ScoreServiceV1_1 ssV1 = new ScoreServiceV1_1();
 		
 		ssV1.makeScore();
 		ssV1.makeSum();
---
+++ Java_008_Application/src/com/callor/apps/service/ScoreServiceV1_1.java
@@ -0,0 +1,65 @@
+package com.callor.apps.service;
+
+import java.util.Scanner;
+
+public class ScoreServiceV1_1 {
+
+	int[] intKors = new int[5];
+	int[] intEngs = new int[intKors.length];
+	int[] intMaths = new int[intKors.length];
+
+	int[] intTotal = new int[intKors.length];
+	float[] floatAvg = new float[intKors.length];
+
+	Scanner scan = new Scanner(System.in);
+	int lineLength = 47;
+
+	public void makeScore() {
+
+		System.out.println(LinesService.dLines(lineLength));
+		for (int i = 0; i < intKors.length; i++) {
+
+			System.out.printf("%d번 학생의 성적을 입력하세요\n", i + 1);
+			System.out.println();
+			System.out.print("국어>> ");
+			intKors[i] = scan.nextInt();
+			System.out.print("영어>> ");
+			intEngs[i] = scan.nextInt();
+			System.out.print("수학>> ");
+			intMaths[i] = scan.nextInt();
+			System.out.println(LinesService.dLines(lineLength));
+		}
+
+	}
+
+	public void makeSum() {
+
+		for (int i = 0; i < intKors.length; i++) {
+			intTotal[i] = intKors[i];
+			intTotal[i] += intEngs[i];
+			intTotal[i] += intMaths[i];
+
+			floatAvg[i] = (float) intTotal[i] / 3;
+		}
+	}
+
+	public void printList() {
+
+		System.out.println("\t\t세라여고 성적표");
+		System.out.println(LinesService.dLines(lineLength));
+		System.out.println("학번\t국어\t영어\t수학\t총점\t평균");
+		System.out.println(LinesService.sLines(lineLength));
+
+		for (int i = 0; i < intKors.length; i++) {
+			System.out.printf("%3d\t", (i + 1));
+			System.out.printf("%3d\t", intKors[i]);
+			System.out.printf("%3d\t", intEngs[i]);
+			System.out.printf("%3d\t", intMaths[i]);
+
+			System.out.printf("%3d\t", intTotal[i]);
+			System.out.printf("%3.2f\t\n", floatAvg[i]);
+		}
+
+		System.out.println(LinesService.dLines(lineLength));
+	}
+}
